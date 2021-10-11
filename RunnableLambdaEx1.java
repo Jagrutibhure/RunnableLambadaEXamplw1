1380af877cb0986ac7a4806fb9f4a5d161f1ba96
Jagruti Bhure <Jagruti@VERINITE1.local>
2021-10-11 20:38:19 +0530

    Runnable lambda Expression

---
+++ RunnableLambdaEx1.java
@@ -0,0 +1,20 @@
+package RunnableLambada;
+
+public class RunnableLambdaEx1 {
+	public static void main (String args[]) {
+		Runnable r1 = new Runnable () {
+			@Override
+			public void run() {
+				System.out.println("Inside Runnable1");
+			}
+		};
+			Runnable r2 = () -> {
+				System.out.println(" Runnable lambda expression");
+				
+			};
+
+			new Thread (r1).start();
+		     new Thread (r2).start();
+	}
+	}
+
